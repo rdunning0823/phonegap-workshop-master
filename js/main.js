df1298863c5b47a98c8367a56a3d24da7521b14c
Rob Dunning <rob@raspberryridgesheepfarm.com>
2017-03-02 13:34:58 -0500

    add alerts with DEBUG

--- js/main.js
+++ js/main.js
@@ -12,9 +12,22 @@ var app = {
             }
         });
     },
+    
+    showAlert: function (message, title) {
+        alert('showAlert() ENTER');
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
 
     initialize: function() {
-        this.store = new WebSqlStore();
+        this.store = new WebSqlStore(function() {
+            alert('callback function ENTER');
+            app.showAlert('Store Initialized', 'Info');
+            alert('callback function EXIT');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
