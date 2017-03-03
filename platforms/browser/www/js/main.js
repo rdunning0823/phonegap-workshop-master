85f56215eebc845bc63c3acf138b05364171eb14
Rob Dunning <rob@raspberryridgesheepfarm.com>
2017-03-02 22:53:02 -0500

    create www folder.  generate platforms & plugins folders w/ phonegap app

---
+++ platforms/browser/www/js/main.js
@@ -0,0 +1,35 @@
+var app = {
+
+    findByName: function() {
+        console.log('findByName');
+        this.store.findByName($('.search-key').val(), function(employees) {
+            var l = employees.length;
+            var e;
+            $('.employee-list').empty();
+            for (var i=0; i<l; i++) {
+                e = employees[i];
+                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+            }
+        });
+    },
+    
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            alert('showing native alert next');
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert('showing browser alert next');
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
+    initialize: function() {
+        this.store = new WebSqlStore(function() {
+            app.showAlert('Store Initialized', 'Info');
+        });
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    }
+
+};
+
+app.initialize();
